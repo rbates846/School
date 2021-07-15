6d68427d1f6a993f6f6126f7565f2a25416ee097
yapdgunasekara <yapdgunasekara@students.nsbm.ac.lk>
2021-07-15 20:48:42 +0530

    New Commit

--- School Management/ManageStaff.Designer.cs
+++ School Management/ManageStaff.Designer.cs
@@ -149,7 +149,7 @@ namespace School_Management
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(333, 196);
             this.dataGridView1.TabIndex = 35;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            
             // 
             // Delete
             // 
